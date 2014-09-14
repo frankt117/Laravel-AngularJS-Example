d11e24ec888b9e3aa328f0214b5ab546628092a2
Frank <root@symposium.webdevblogs.com>
2014-09-14 13:02:26 -0500

    added comment controller

---
+++ app/controllers/CommentController.php
@@ -0,0 +1,39 @@
+<?php
+
+class CommentController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
