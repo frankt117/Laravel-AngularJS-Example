5543a0f1162a75ab562b75b586433d164b953441
frankt117 <frankjtorresjr@gmail.com>
2014-09-14 14:21:40 -0500

    fixed spacing.

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -2,45 +2,45 @@
 
 class CommentController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return Response::json(Comment::get());
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    return Response::json(Comment::get());
+  }
 
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		Comment::create(array(
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+    Comment::create(array(
       'author' => Input::get('author'),
       'text' => Input::get('text')
     ));
 
     return Response::json(array('success' => true));
-	}
+  }
 
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		Comment::destroy($id);
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    Comment::destroy($id);
 
     return Response::json(array('success' => true));
-	}
+  }
 
 
 }
